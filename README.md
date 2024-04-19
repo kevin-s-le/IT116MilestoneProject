2e062b59746da54e9db0313342f1a1a2c5895ef6
kevin-s-le <kevin.s.le@seattlecolleges.edu>
2024-04-18 22:45:42 -0700

    Moved Index and About to parent folder, added scrolling Navbar, adding video to index page

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # IT116MilestoneProject
+
+Welcome to our IT 116 Group Project!
+Worked on by Alex, Kevin, Matdeli, and Nakyoung.
---
+++ code/script.js
@@ -0,0 +1,14 @@
+// JavaScript can be used to interact with the video player, such as playing, pausing, or controlling playback
+
+// Get the video element
+const video = document.getElementById('brandVideo');
+
+// Play the video
+function playVideo() {
+    video.play();
+}
+
+// Pause the video
+function pauseVideo() {
+    video.pause();
+}
